6790f3615cccda4d0d1527893dadcd5d98bb7ee6
suhrabpeaky <93655364+suhrabpeaky@users.noreply.github.com>
2022-02-21 15:52:20 +0500

    new one

--- main.js
+++
@@ -1,8 +0,0 @@
-const text = document.querySelector('.text');
-
-text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
-
-text.addEventListener('mouseover', function(event) {
-    if (event.target.tagName != 'SPAN') return;
-    event.target.classList.add('active');
-});
